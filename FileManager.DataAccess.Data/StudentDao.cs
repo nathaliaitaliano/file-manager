e35e375646e1158eac56ff93ff9e94e1e3c0f76d
nathalia-italiano <nathalia.italiano@atmira.com>
2020-02-29 15:46:12 +0100

    initial commit close #8
    FileManager project with structure related to three tiers architecture

---
+++ FileManager.DataAccess.Data/StudentDao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FileManager.Common.Layer;
+
+namespace FileManager.DataAccess.Data
+{
+    public class StudentDao
+    {
+        public Student Add(Student student)
+        {
+            throw new NotImplementedException(
+                "The method is not implemented yet");
+        }
+
+    }
+}
