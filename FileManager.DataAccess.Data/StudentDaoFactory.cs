910615bcedf16d54bae51c11c0454c63a1792134
nathalia-italiano <nathalia.italiano@atmira.com>
2020-03-03 02:29:01 +0100

    add multiple students in a xml file close #16
    multiple students are added through a list

--- FileManager.DataAccess.Data/StudentDaoFactory.cs
+++ FileManager.DataAccess.Data/StudentDaoFactory.cs
@@ -1,9 +1,5 @@
 ﻿using FileManager.Presentation.WinSite;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FileManager.DataAccess.Data
 {
--- FileManager.DataAccess.Data/XmlStudentDao.cs
+++ FileManager.DataAccess.Data/XmlStudentDao.cs
@@ -1,4 +1,5 @@
 ﻿using FileManager.Common.Layer;
+using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
 
@@ -6,13 +7,40 @@ namespace FileManager.DataAccess.Data
 {
     public class XmlStudentDao : StudentDao
     {
+        public XmlStudentDao()
+        {
+            if (!File.Exists(@"C:\Users\usuario\source\repos\FileManager\FileRepository\XMLStudantDao.xml"))
+            {
+                File.Create(@"C:\Users\usuario\source\repos\FileManager\FileRepository\XMLStudantDao.xml").Close();
+                WriteToFile(new List<Student>());
+            }
+            
+        }
         public override void Add(Student student)
         {
-            XmlSerializer serializer = new XmlSerializer(typeof(Student));
+            List<Student> studentList = ReadFromFile();
+            studentList.Add(student);
+            WriteToFile(studentList);
+        }
+
+        private void WriteToFile(List<Student> studentList)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Student>));
             using (TextWriter writer = new StreamWriter(@"C:\Users\usuario\source\repos\FileManager\FileRepository\XMLStudantDao.xml"))
             {
-                serializer.Serialize(writer, student);
+                serializer.Serialize(writer, studentList);
             }
+        }
+
+        private List<Student> ReadFromFile()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(List<Student>));
+            TextReader reader = new StreamReader(@"C:\Users\usuario\source\repos\FileManager\FileRepository\XMLStudantDao.xml");
+            object obj = deserializer.Deserialize(reader);
+            List<Student> studentList = (List<Student>)obj;
+            reader.Close();
+
+            return studentList;
         }
     }
 }
