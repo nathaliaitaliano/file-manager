1a79b1000ea5b8a441e2024b383242fc0a2c3f6c
nathalia-italiano <nathalia.italiano@atmira.com>
2020-03-03 02:59:23 +0100

    add multiple students in a json file close #17
    multiple students are added through a list

---
+++ FileManager.DataAccess.Data/JsonStudantDao.cs
@@ -0,0 +1,43 @@
+﻿using FileManager.Common.Layer;
+using FileManager.DataAccess.Data;
+using System.Collections.Generic;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace FileManager.Presentation.WinSite
+{
+    public class JsonStudentDao : StudentDao
+    {
+        public JsonStudentDao()
+        {
+            if (!File.Exists(@"C:\Users\usuario\source\repos\FileManager\FileRepository\JSONtudantDao.json"))
+            {
+                File.Create(@"C:\Users\usuario\source\repos\FileManager\FileRepository\JSONtudantDao.json").Close();
+                WriteToFile(new List<Student>());
+            }
+        }
+
+        public override void Add(Student student)
+        {
+            List<Student> studentList = ReadFromFile();
+            studentList.Add(student);
+            WriteToFile(studentList);
+        }
+
+        private void WriteToFile(List<Student> studentList)
+        {
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            using (TextWriter writer = new StreamWriter(@"C:\Users\usuario\source\repos\FileManager\FileRepository\JSONtudantDao.json"))
+            {
+                writer.WriteLine(serializer.Serialize(studentList));
+            }
+        }
+
+        private List<Student> ReadFromFile()
+        {
+            JavaScriptSerializer deserializer = new JavaScriptSerializer();
+            string jsonContent = File.ReadAllText(@"C:\Users\usuario\source\repos\FileManager\FileRepository\JSONtudantDao.json");
+            return (List<Student>)deserializer.Deserialize(jsonContent, typeof(List<Student>));
+        }
+    }
+}
